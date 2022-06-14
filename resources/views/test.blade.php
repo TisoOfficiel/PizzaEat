9fd7a8d5330eb249f388ddc68fbde1d6ac27dacd
Romain L <51294979+TisoOfficiel@users.noreply.github.com>
2022-06-14 12:07:09 +0200

    Delete resources directory

--- resources/views/test.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.app')
-
-@section('content')
-      
-
-    <img src="storage\img_pizzatest\{{pizza->id}}.png" alt="">
-@endsection
--- resources/views/upload/form.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.app')
-@section('title','File upload')
-@section('content')
-<form action="{{route('fupload')}}" method="post" enctype="multipart/form-data">
-<input type="file" name="fichier">
-<input type="submit" value="Téléverser">
-@csrf
-</form>
-@endsection
